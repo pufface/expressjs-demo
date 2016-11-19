9ba3fc55f92299bec7be02c2c2f1b528a958d186
Peter Fillo <peterfillo@bwtplus.sk>
2016-11-19 19:26:35 +0100

    express route regexp, next callback

---
+++ helper_create_users.js
@@ -0,0 +1,30 @@
+var fs = require('fs')
+var _ = require('lodash')
+
+var createUser = function(firstName, lastName) {
+  var isFemale = Math.random() > 0.5
+  return {
+    gender: isFemale ? 'female' : 'male',
+    name: {
+      title: isFemale ? (Math.random() > 0.5 ? 'miss' : 'mrs') : 'mr',
+      first: firstName,
+      last: lastName
+    },
+    username: _.toLower(firstName + lastName),
+    email: firstName + '.' + lastName + '@mail.com'
+  }
+}
+
+var users = []
+users.push(createUser('Mary', 'Jones'))
+users.push(createUser('Alan', 'Walter'))
+users.push(createUser('Miriam', 'Wallace'))
+users.push(createUser('william', 'Ryan'))
+users.push(createUser('Gail', 'Morales'))
+users.push(createUser('Big', 'First'))
+users.push(createUser('Big', 'Second'))
+
+fs.writeFile('./users.json', JSON.stringify(users), function(err) {
+  if (err) throw err
+  console.log('Users created');
+})
